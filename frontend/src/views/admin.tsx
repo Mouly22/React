0aaa03a193c664db5cbaa31c1f6fc2ce91ebc9c7
MD Ikramul Kayes <ikramul.kayesgg@gmail.com>
2023-11-13 00:46:41 +0600

    gg

--- frontend/src/views/admin.tsx
+++ frontend/src/views/admin.tsx
@@ -4,33 +4,32 @@ import Admin_Page_show_admins from './admin_show_admins';
 import Admin_Page_show_Field_Officers from './admin_show_fieldofficers_list';
 import Admin_Page_show_Incoming_request from './admin_show_incoming_request';
 import SignupForm from '../Pages/SignUp';
-//import { Link } from 'react-router-dom';
-
 import AddAdminsButton from './admin_show_button_for_adding_admins';
+
 const Admin_Page: React.FC = () => {
   const [userid, setUserid] = useState('');
   const [userType, setUserType] = useState('');
+
   useEffect(() => {
     const storedUserId = localStorage.getItem('userid');
     const storedUserType = localStorage.getItem('user_type');
     setUserid(storedUserId || '');
-    setUserType(storedUserType || '');});
-  return(
-    <>
+    setUserType(storedUserType || '');
+  });
+
+  return (
+    <div style={{ maxHeight: '80vh', overflowY: 'auto' }}>
       <div>
         <h2>Welcome, Admin</h2>
         <p>User ID: {userid}</p>
         <p>User Type: {userType}</p>
       </div>
       <Admin_Page_show_Incoming_request></Admin_Page_show_Incoming_request>
-    <Admin_Page_show_admins></Admin_Page_show_admins>
-    <Admin_Page_show_Field_Officers></Admin_Page_show_Field_Officers>
-    <AddAdminsButton></AddAdminsButton>
-    
-    </>
-  )
-
-
+      <Admin_Page_show_admins></Admin_Page_show_admins>
+      <Admin_Page_show_Field_Officers></Admin_Page_show_Field_Officers>
+      <AddAdminsButton></AddAdminsButton>
+    </div>
+  );
 };
 
 export default Admin_Page;
