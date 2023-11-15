ad5e7139e809fd4a980ad8e7a2b5e34f57b7f76a
Mouly <abiraazmary22@gmail.com>
2023-11-15 22:03:47 +0600

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,7 @@ import Blog from "./Pages/BlogF/Blog"
 import './App.css';
 import SinglePost from "./Pages/BlogF/singlePost"
 import Write from "./Pages/BlogF/write"
+import Sidebar from "./Pages/BlogF/Sidebar";
 //import Field_Officer_Page from "./views/field_officer";
 import Field_officer_Page from "./views_field_officers/Field_officer_view";
 import SignupForm_for_admin from "./views/admin_show_admin_adding_page";
@@ -25,6 +26,9 @@ function App() {
             <Route path="/Blog" element={<Blog/>}/>
             <Route path = "/singlePost/:postId" element= {<SinglePost/>}/>
             <Route path = "/write" element= {<Write/>}/>
+           
+            
+            
 
          
             
