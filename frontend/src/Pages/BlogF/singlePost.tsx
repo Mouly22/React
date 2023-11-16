08c2e224fc72d2824289cbd6bfde205d525c34c7
MD Ikramul Kayes <ikramul.kayesgg@gmail.com>
2023-11-16 10:52:08 +0600

    added singleposts

--- frontend/src/Pages/BlogF/singlePost.tsx
+++ frontend/src/Pages/BlogF/singlePost.tsx
@@ -5,6 +5,7 @@ import { useParams } from "react-router-dom";
 const SinglePost: React.FC<{}> = () => {
   const { postId } = useParams<{ postId: string }>();
   const [postData, setPostData] = useState<any>(null);
+  const [imageSrc, setImageSrc] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -13,9 +14,28 @@ const SinglePost: React.FC<{}> = () => {
           post_id: postId,
         });
         setPostData(response.data.user_data);
+
+        // Fetch image
+        const imageResponse = await axios.post(
+          "http://127.0.0.1:8000/login_blog_images/",
+          {
+            post_id: postId,
+          },
+          {
+            responseType: 'arraybuffer',
+          }
+        );
+
+        const base64 = btoa(
+          new Uint8Array(imageResponse.data).reduce(
+            (data, byte) => data + String.fromCharCode(byte),
+            ''
+          )
+        );
+
+        setImageSrc(`data:${imageResponse.headers['content-type']};base64,${base64}`);
       } catch (error) {
-        console.error("Error fetching post data:", error);
-        // Handle errors (e.g., set a default value for postData)
+        console.error("Error fetching data:", error);
       }
     };
 
@@ -25,18 +45,26 @@ const SinglePost: React.FC<{}> = () => {
   return (
     <div>
       {postData && (
-        <>
+        <div>
           <h1>{postData.post_title}</h1>
           <p>{postData.post_content}</p>
-          <p> Post was uploaded on: {postData.post_uploaded}</p>
-          <p>{postData.post_image}</p>
+          <p>Uploaded on: {new Date(postData.post_uploaded).toLocaleString()}</p>
+          {imageSrc && <img src={imageSrc} alt="Post Image" style={{ maxWidth: "100%" }} />}
+          <div>
+            <h2>Comments</h2>
+            <ul>
+              {postData.comments.map((comment: any) => (
+                <li key={comment.comment_id}>
+                  <strong>{comment.userid}:</strong> {comment.comment_content}
+                </li>
+              ))}
+            </ul>
+          </div>
           {/* Add more details as needed */}
-        </>
+        </div>
       )}
     </div>
   );
 };
 
 export default SinglePost;
-
-
