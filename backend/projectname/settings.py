85be15f1d908b057c1dafedc797026aa97d0cfbd
NaimParves <106744976+NaimParves@users.noreply.github.com>
2023-11-21 03:24:52 +0600

    updated bal, git pari na

--- backend/projectname/settings.py
+++ backend/projectname/settings.py
@@ -101,9 +101,9 @@ DATABASES = {
 'ENGINE': 'django.db.backends.mysql',
 'NAME': 'sample2', # database name
 'USER': 'root',
-'PASSWORD': 'root',
-'HOST': '172.17.0.2', # or the hostname where your MySQL server is running
-'PORT': '', # or the port on which your MySQL server is listening
+'PASSWORD': '',
+'HOST': 'localhost', # or the hostname where your MySQL server is running
+'PORT': '3306', # or the port on which your MySQL server is listening
 }
 }
 
