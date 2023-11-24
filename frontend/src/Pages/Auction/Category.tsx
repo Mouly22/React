fa505d9f7aec6cb89c67f1c9348199eb7e819e90
MD Ikramul Kayes <ikramul.kayesgg@gmail.com>
2023-11-25 01:30:44 +0600

    pending tasks for sortings

--- frontend/src/Pages/Auction/Category.tsx
+++ frontend/src/Pages/Auction/Category.tsx
@@ -1,36 +1,57 @@
 import React from "react";
-import "./Category.css"
+import "./Category.css";
 
 const Category: React.FC<{}> = () => {
-    return (
-        <div>
+  const handleCategoryChange = (selectedCategory: string) => {
+    // Update the value in localStorage
+    console.log(selectedCategory);
+    localStorage.setItem("type", selectedCategory);
+  };
 
-
-        <h3 className = "sidebar-title"><span className="sidebarTitle">Category 🛒 </span></h3>
-            <div>
-            <label className = "sidebar-label-container">
-                
-                <input type= "radio" name = "test"/>
-                <span className = "checkmark"></span>All
-            </label>
-            <br/>
-            <label className = "sidebar-label-container">
-                <input type= "radio" name = "test"/>
-                <span className = "checkmark"></span>Crops
-            </label>
-            <br/>
-            <label className = "sidebar-label-container">
-                <input type= "radio" name = "test"/>
-                <span className = "checkmark"></span>Vegetables
-            </label>
-            <br/>
-            <label className = "sidebar-label-container">
-                <input type= "radio" name = "test"/>
-                <span className = "checkmark"></span>Fruits
-            </label>
-            </div>
-        </div>
-        
-    );
+  return (
+    <div>
+      <h3 className="sidebar-title">
+        <span className="sidebarTitle">Category 🛒 </span>
+      </h3>
+      <div>
+        <label className="sidebar-label-container">
+          <input
+            type="radio"
+            name="test"
+            onChange={() => handleCategoryChange("")}
+          />
+          <span className="checkmark"></span>All
+        </label>
+        <br />
+        <label className="sidebar-label-container">
+          <input
+            type="radio"
+            name="test"
+            onChange={() => handleCategoryChange("Crops")}
+          />
+          <span className="checkmark"></span>Crops
+        </label>
+        <br />
+        <label className="sidebar-label-container">
+          <input
+            type="radio"
+            name="test"
+            onChange={() => handleCategoryChange("Vegetables")}
+          />
+          <span className="checkmark"></span>Vegetables
+        </label>
+        <br />
+        <label className="sidebar-label-container">
+          <input
+            type="radio"
+            name="test"
+            onChange={() => handleCategoryChange("Fruits")}
+          />
+          <span className="checkmark"></span>Fruits
+        </label>
+      </div>
+    </div>
+  );
 };
+
 export default Category;
--- frontend/src/Pages/Auction/Price.tsx
+++ frontend/src/Pages/Auction/Price.tsx
@@ -1,31 +1,55 @@
 import React from "react";
 
-
 const Price: React.FC<{}> = () => {
-    return (
-        <div>
-        <h3 className = "sidebar-title price-title"><span className="sidebarTitle">Price </span></h3>
+  const handlePriceChange = (minPrice: number, maxPrice: number) => {
+    // Update the value in localStorage
+    console.log("price_range", `${minPrice},${maxPrice}`)
+    localStorage.setItem("price_range", `${minPrice},${maxPrice}`);
+  };
 
-        <label className = "sidebar-label-container">
-            <input type="radio" name = "test2"/>
-            <span className = "checkmark"></span>All
-        </label>
-        <br/>
-        <label className = "sidebar-label-container">
-            <input type="radio" name = "test2"/>
-            <span className = "checkmark"></span>$0 - $150
-        </label>
-        <br/>
-        <label className = "sidebar-label-container">
-            <input type="radio" name = "test2"/>
-            <span className = "checkmark"></span>$150 - $300
-        </label>
-        <br/>
-        <label className = "sidebar-label-container">
-            <input type="radio" name = "test2"/>
-            <span className = "checkmark"></span>$300+
-        </label>
-        </div>
-    );
+  return (
+    <div>
+      <h3 className="sidebar-title price-title">
+        <span className="sidebarTitle">Price </span>
+      </h3>
+
+      <label className="sidebar-label-container">
+        <input
+          type="radio"
+          name="test2"
+          onChange={() => handlePriceChange(0, 9999999)} // Set your min and max price values
+        />
+        <span className="checkmark"></span>All
+      </label>
+      <br />
+      <label className="sidebar-label-container">
+        <input
+          type="radio"
+          name="test2"
+          onChange={() => handlePriceChange(0, 15000)}
+        />
+        <span className="checkmark"></span>$0 - $15000
+      </label>
+      <br />
+      <label className="sidebar-label-container">
+        <input
+          type="radio"
+          name="test2"
+          onChange={() => handlePriceChange(15000, 30000)}
+        />
+        <span className="checkmark"></span>$15000 - $30000
+      </label>
+      <br />
+      <label className="sidebar-label-container">
+        <input
+          type="radio"
+          name="test2"
+          onChange={() => handlePriceChange(30000, 9999999)} // You can set a large value for Infinity
+        />
+        <span className="checkmark"></span>$30000+
+      </label>
+    </div>
+  );
 };
+
 export default Price;
